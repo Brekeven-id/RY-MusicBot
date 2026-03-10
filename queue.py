377fe6d4a04eb5ec8bfd2436dd59a07424a12a7e
Brekeven-id <jihanfahtini@gmail.com>
2026-03-10 12:20:02 +0700

    Update queue.py

--- queue.py
+++ queue.py
@@ -1,9 +1,11 @@
 queues = {}
 
-def add_song(chat_id, song):
-    if chat_id not in queues:
-        queues[chat_id] = []
-    queues[chat_id].append(song)
+def add(chat_id, song):
+    queues.setdefault(chat_id, []).append(song)
 
-def get_queue(chat_id):
-    return queues.get(chat_id, [])
+def get(chat_id):
+    return queues.get(chat_id)
+
+def pop(chat_id):
+    if chat_id in queues and queues[chat_id]:
+        queues[chat_id].pop(0)
