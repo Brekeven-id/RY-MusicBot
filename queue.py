590b717390aa5b6a21e8c430072707e0b4f1b1a3
Brekeven-id <jihanfahtini@gmail.com>
2026-03-10 13:36:16 +0700

    Update queue.py

--- queue.py
+++ queue.py
@@ -4,8 +4,7 @@ def add(chat_id, song):
     queues.setdefault(chat_id, []).append(song)
 
 def get(chat_id):
-    return queues.get(chat_id)
+    return queues.get(chat_id, [])
 
-def pop(chat_id):
-    if chat_id in queues and queues[chat_id]:
-        queues[chat_id].pop(0)
+def clear(chat_id):
+    queues[chat_id] = []
